1bbe15e61a11e6081ded1fd7e63f7be61617f85d
Konstantin Nikolaenko <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2022-07-29 22:29:22 +0300

    Update BookDAO.java

--- src/main/java/ru/alishev/springcourse/dao/BookDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/BookDAO.java
@@ -19,39 +19,47 @@ public class BookDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    //Select all books
     public List<Book> index() {
         return jdbcTemplate.query("SELECT id_book, title, author, year_of_writing FROM Book",
                 new BeanPropertyRowMapper<>(Book.class));
     }
 
+    //Update book, free
     public void freeBook(int id){
         jdbcTemplate.update("UPDATE book SET id_person=null WHERE id_book=?", id);
     }
 
+    //Book id search 
     public Book showIdBook(int id) {
         return (Book) jdbcTemplate.query("SELECT * FROM book WHERE id_book=? ", new Object[]{id},
                 new BeanPropertyRowMapper<>(Book.class)).stream().findAny().orElse(null);
     }
 
+    //Appoint book, update
     public void bookAppoint(int idBook, int idPerson){
         jdbcTemplate.update("UPDATE book SET id_person=? WHERE id_book=?", idPerson, idBook);
     }
 
+    //Insert book
     public void save(Book book) {
         jdbcTemplate.update("INSERT INTO book(title, author, year_of_writing) VALUES(?, ?, ?)",
                 book.getTitle(), book.getAuthor(), book.getYearOfWriting());
     }
 
+    //Edit book
     public void update(int id, Book book) {
         jdbcTemplate.update("UPDATE book SET title=?, author=?, year_of_writing=? WHERE id_book=?",
                 book.getTitle(), book.getAuthor(), book.getYearOfWriting(), id);
     }
 
+    //Select get book Owner
     public Optional<Person> getBookOwner(int id){
         return jdbcTemplate.query("SELECT person.* FROM book JOIN person ON book.id_person=person.id_person " +
                 "WHERE id_book=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 
+    //Delete book
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM book WHERE id_book=?", id);
     }
