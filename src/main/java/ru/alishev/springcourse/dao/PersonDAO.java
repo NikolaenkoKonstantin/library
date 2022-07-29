a5f960f756ce1492c9fcc672fd92253cae29fc9f
Konstantin Nikolaenko <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2022-07-29 22:31:43 +0300

    Update PersonDAO.java

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -20,36 +20,42 @@ public class PersonDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    //Output people
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
+    //Select get books by person id
     public List<Book> getBooksByPersonId(int id) {
         return jdbcTemplate.query("SELECT * FROM book WHERE id_person=? ", new Object[]{id},
                 new BeanPropertyRowMapper<>(Book.class));
     }
 
+    //Select person by id
     public Optional<Person> show(int id) {
         return jdbcTemplate.query("SELECT * FROM Person WHERE id_person=?", new Object[]{id},
                 new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 
+    //Insert person
     public void save(Person person) {
         jdbcTemplate.update("INSERT INTO Person(fio, year_of_birth) VALUES(?, ?)",
                 person.getFio(), person.getYearOfBirth());
     }
 
+    //Edit person
     public void update(int id, Person updatedPerson) {
         jdbcTemplate.update("UPDATE Person SET fio=?, year_of_birth=? WHERE id_person=?",
                 updatedPerson.getFio(), updatedPerson.getYearOfBirth(), id);
     }
 
-    // Для валидации уникальности ФИО
+    //To validate the uniqueness of the full name
     public Optional<Person> getPersonByFullName(String fullName) {
         return jdbcTemplate.query("SELECT * FROM Person WHERE fio=?", new Object[]{fullName},
                 new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 
+    //Delete person
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id_person=?", id);
     }
